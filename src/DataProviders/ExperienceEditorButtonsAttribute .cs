e70016634f8a4d7a40c98a417181fc448aaa11d0
Arjen van Veen <arjenvanveen@gmail.com>
2015-08-19 11:28:30 +0200

    Added ExpierenceEditorButtons Attribute

---
+++ src/DataProviders/ExperienceEditorButtonsAttribute .cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BoC.Sitecore.CodeFirstRenderings.DataProviders
+{
+    public class ExperienceEditorButtonsAttribute : Attribute
+    {
+        public string ExperienceEditorButtons { get; set; }
+
+        public ExperienceEditorButtonsAttribute(string experienceEditorButtons)
+        {
+            this.ExperienceEditorButtons = experienceEditorButtons;
+        }
+    }
+}
